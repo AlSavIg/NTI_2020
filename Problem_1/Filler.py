d74e7d73e9941def41bb71596b31f909117b34de
Aleksandr <56115945+AlSavIg@users.noreply.github.com+alesavigo@gmail.com>
2021-03-09 18:09:15 +0700

    Completed!

---
+++ Problem_1/Filler.py
@@ -0,0 +1,17 @@
+from random import randint
+
+height = randint(5, 15)
+width = randint(5, 15)
+file_output = open("input.txt", "w")
+file_output.write(str(height) + " " + str(width) + "\n")
+for i in range(height):
+    if i == 0 or i == height - 1:
+        for j in range(width):
+            file_output.write("0" + " ")
+    else:
+        for j in range(width):
+            if j == 0 or j == width - 1:
+                file_output.write("0" + " ")
+            else:
+                file_output.write(str(randint(0, 1)) + " ")
+    file_output.write("\n")
